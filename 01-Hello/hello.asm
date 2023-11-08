c73ff4cbc351f08b3f54b17fb25d095f2f1260e0
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2023-11-08 14:05:30 -0300

    add comment in C language

--- 01-Hello/hello.asm
+++ 01-Hello/hello.asm
@@ -1,49 +1,54 @@
-section .data
-    SYS_WRITE equ 1
-    SYS_READ  equ 0
-    STDOUT    equ 1
-    STDIN     equ 0
-    SYS_EXIT  equ 60
-    EXIT      equ 0
+; Intel - IA-32 (32bits) - compatível com x86-64 (64 bits)
 
-    QUESTION     db "Qual seu nome? "
-    SIZE_QUEST  equ $-QUESTION
-    HELLO        db "Olá, "
-    SIZE_HELLO  equ $-HELLO
-    SIZE_NAME     equ 10
+segment .data
+    ; EAX
+    SYS_READ  equ 0x3
+    SYS_WRITE equ 0x4
+    SYS_EXIT  equ 0x3c
+
+    ;EBX
+    STD_IN    equ 0x0
+    STD_OUT   equ 0x1
+    EXIT      equ 0x0
+
+section .data
+    QUESTION     db "Qual seu nome? "   ; char QUESTION[] = "Qual seu nome?";
+    SIZE_QUEST  equ $ - QUESTION        ; int SIZE_QUESTION = 14;
+    HELLO        db "Olá, "             ; char HELLO[] = "Olá, ";
+    SIZE_HELLO  equ $-HELLO             ; int SIZE_HELLO = 5;
+    SIZE_NAME     equ 0xA               ; int SIZE_NAME = 10;
 
 section .bss
-    NAME resb SIZE_NAME
+    NAME resb SIZE_NAME                 ; char NAME[SIZE_NAME];
 
 section .text
     global _start
 
 _start:
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, QUESTION
+    mov eax, SYS_WRITE                  ; printf("%s", &QUESTION);
+    mov rbx, STD_OUT
+    mov rcx, QUESTION
     mov rdx, SIZE_QUEST
     syscall
 
-    mov rax, SYS_READ
-    mov rdi, STDIN
-    mov rsi, NAME
+    mov rax, SYS_READ                   ; scanf(%s, &NAME);
+    mov rbx, STD_IN
+    mov rcx, NAME
     mov rdx, SIZE_NAME
     syscall
 
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, HELLO
+    mov rax, SYS_WRITE                  ; printf("%s", &HELLO);
+    mov rbx, STD_OUT
+    mov rcx, HELLO
     mov rdx, SIZE_HELLO
     syscall
 
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, NAME
+    mov rax, SYS_WRITE                  ; printf("%s", &NAME);
+    mov rbx, STD_OUT
+    mov rcx, NAME
     mov rdx, SIZE_NAME
     syscall
 
-
-    mov rax, SYS_EXIT
-    mov rdi, EXIT
+    mov rax, SYS_EXIT                   ; return 0;
+    mov rbx, EXIT
     syscall
