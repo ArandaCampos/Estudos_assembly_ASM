690db2bb2547b95ca2303f690ebf05780a3e3a76
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2023-11-11 23:39:50 -0300

    add - is prime program

---
+++ 04-prime/prime.asm
@@ -0,0 +1,51 @@
+; x86 (Intel/ AMD - 32/64 bits)
+
+section .rdata
+    SYS_EXIT    equ 0x1
+    SYS_CALL    equ 0x80
+    EXIT        equ 0x0
+
+section .data
+    N           equ 0x61    ; 97 (base 10)
+
+section .text
+
+global _start
+
+
+is_prime:
+    cmp ebx, ecx        ; if (n <= 1)
+        jle .negative   ;       return 0;
+
+    mov ecx, 0x1        ; counter = 2;
+
+.FOR:                   ; for
+                        ; {
+    inc ecx             ;   divisor++
+    cmp ecx, ebx        ;   if (n == divisor)
+        je .positive    ;       return 1;
+
+    mov eax, ebx
+    div ecx             ;   quociente = n / divisor; resto = n % divisor
+
+    cmp edx, 0          ;   if (resto == 0)
+        je .negative    ;       return 0;
+
+    xor edx, edx
+    jmp .FOR            ; }
+
+.negative:
+    mov eax, 0
+    ret
+.positive:
+    mov eax, 1
+    ret
+
+_start:
+
+    mov ebx, N
+    call is_prime
+
+    mov eax, SYS_EXIT
+    mov ebx, EXIT
+    int SYS_CALL
