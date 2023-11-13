a33ec52fe9f2ebbbfba0e1867d47d1406f78975e
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2023-11-13 15:54:09 -0300

    modified - replace comment in C to comment in portugol and modified list

--- 06-linear_search/linearsearch.asm
+++ 06-linear_search/linearsearch.asm
@@ -5,57 +5,40 @@ section .rdata
     SYS_CALL    equ 0x80
     EXIT        equ 0x0
 
-    TAM_MAX     equ     0x32    ; 50 (base 10)
-
-section .bss
-    list resd TAM_MAX
+    LIST        db 0xA, 0xF, 0x11, 0x14     ; [10, 15, 17, 20]
+    SIZE        equ $-LIST
 
 section .text
 
 global _start
 
-linearsearch:           ; int linearsearch(int* list, int* end, int n) {
-    lea edx, list       ;       int *aux = list;
-.next:                  ;       while (1){
-    cmp [edx], ecx      ;           if (*list == n )
-        je .found       ;               return aux;
-    add edx, 4          ;           list = list->next;
-    cmp edx, eax        ;           if (list == fim)
-        je .notfound    ;               return 0;
-    jmp .next           ;       }
-                        ; }
+linearsearch:               ; EAX = &lista, EBX = Número buscado, ECX = SIZE
+    cmp ecx, 0x0            ; se lista vazia:
+        je .notfound        ;       retorna 0;
+    mov esi, eax            ;
+    xor eax, eax            ; contador (eax) = 0
+.next:                      ; enquanto (SIZE > 0){
+    movzx edx, byte[esi]    ;       edx = lista[0];
+    cmp edx, ebx            ;       if (edx == N )
+        je .found           ;           returna contador;
+    inc esi                 ;       lista = lista[1];
+    inc eax                 ;       contador++;
+    loop .next              ;       }
+                            ; }
 
 .found:
-    mov eax, 1
     ret
 
 .notfound:
     mov eax, 0
     ret
 
-insert:                     ; int* insert(int * pointer, int n) {
-    mov DWORD[eax], edx     ;       *pointer = n;
-    add eax, 4              ;       return pointer->next;
-    ret                     : }
+_start:                     ;
+    lea eax, LIST           ; eax = &lista;
+    mov ebx, 0x11           ; ebx = 17;
+    mov ecx, SIZE           ; ecx = SIZE;
+    call linearsearch       ; linearsearch(eax, ebx, ecx);
 
-_start:                     ; int main (){
-    lea eax, list           ;       int[] *pointer  = list[0];
-
-    mov edx, 0xA            ;       pointer = insert(pointer, 10);
-    call insert
-
-    mov edx, 0XB            ;       pointer = insert (pointer, 11);
-    call insert
-
-    mov edx, 0x1            ;       pointer = insert (pointer, 1);
-    call insert
-
-    mov ecx, 0xA            ;       linearsearch(pointer, 10);
-    call linearsearch
-
-    mov ecx, 0x2            ;       linearsearch(pointer, 2);
-    call linearsearch
-
-    mov eax, SYS_EXIT       ;       return 0;
-    mov ebx, EXIT           ; }
+    mov eax, SYS_EXIT       ; fim_do_programa(0);
+    mov ebx, EXIT           ;
     int SYS_CALL
